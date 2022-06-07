f7a70bb3b39af11fa5b65f8213e6976821cbd88a
arvidio <104368432+arvidio@users.noreply.github.com>
2022-06-07 11:22:54 +0200

    Add files via upload

---
+++ Regex personnummer i betyg.ps1
@@ -0,0 +1,27 @@
+#Script som med hjälp av pdftotext.exe byter namn på filer till det personnummer som finns OCR-at i filen (betyg)
+#https://www.xpdfreader.com/pdftotext-man.html
+#Skrivet för PowerShell 7.2
+
+
+#Detta ändras för att peka på rätt filer
+$filer=get-childitem *.pdf
+
+foreach($fil in $filer){
+    #Detta behöver peka på pdftotext.exe C:\.\pdftotext.exe om den ligger direkt under C till exempel.
+    .\pdftotext.exe -enc UTF-8 $fil.FullName temptext.txt
+    $tempfil = Get-ChildItem temptext.txt
+    $temptext = Get-Content $tempfil
+    $personnummer = $temptext | Out-String -stream | Select-String -Pattern '[\d]{6}-[\d]{4}'
+    if($personnummer -match '[\d]{6}-[\d]{4}'){
+        #Går att förfina för att lägga till lite mer intressant information. 
+        Rename-Item -Path $fil -NewName "$personnummer.pdf"
+    }
+    elseif ($null -eq $personnummer) {
+        Write-Host "Gör inget"
+    }
+    else{
+        Write-Host "Något gick fel"
+    }
+    Remove-Item $tempfil
+}
+ 
