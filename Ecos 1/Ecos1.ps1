423b397cfed9eacfc82c1f3c042938e73da545e4
arvidio <104368432+arvidio@users.noreply.github.com>
2022-06-08 15:19:41 +0200

    commit

---
+++ Ecos 1/Ecos1.ps1
@@ -0,0 +1,26 @@
+
+$csvAr = Import-Csv 'C:\Users\97arer14\Documents\new 2.csv' -Encoding utf8NoBOM -Delimiter ";"
+
+foreach($arende in $csvAr){
+$EgnaElementStart = @"
+<EgnaElement>
+"@
+$EgnaElementSlut = "</EgnaElement>"
+if("NULL" -eq $arende.Fastighetsbeteckning){
+    $EgnaElementBeskrivning = $null
+    $EgnaElementStart = $null
+    $EgnaElementSlut = $null
+    $EgetElement1 = $null
+}
+else{
+    $Fastighetsbeteckning = $arende.Fastighetsbeteckning
+$EgetElement1 = @"
+<EgetElement Namn="Fastighetsbeteckning" DataTyp="String"><Varde>$Fastighetsbeteckning</Varde></EgetElement>
+"@
+$EgnaElementBeskrivning = @"
+<EgnaElementBeskrivning>Det egna elementet beskriver vilken fastighet ärendet berör.</EgnaElementBeskrivning>
+"@
+}
+$EgnaElementTagg = $EgnaElementStart + $EgnaElementBeskrivning + $EgetElement1 + $EgnaElementSlut
+$EgnaElementTagg
+}
