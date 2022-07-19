56b942f9391a5ef4e5c8e6ec25da9ec4da87fe68
arvidio <104368432+arvidio@users.noreply.github.com>
2022-07-19 14:44:18 +0200

    THN

--- Generell filhantering/PrettyPrint.ps1
+++ Generell filhantering/PrettyPrint.ps1
@@ -1,2 +1,2 @@
-$x = [XML](Get-Content .\Documents\Arkivexport\ST2022-07-19TT1049Arkivexport.xml)
+$x = [XML](Get-Content .\Documents\Arkivexport\THN2022-07-19TT1338Arkivexport.xml)
 $x.Save(".\Documents\Prettyprint.xml")
