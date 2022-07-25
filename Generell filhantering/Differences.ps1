0acada9a4bf23509f41f8886d4736e579ff67430
arvidio <104368432+arvidio@users.noreply.github.com>
2022-07-25 14:05:52 +0200

    commit

--- Generell filhantering/Differences.ps1
+++ Generell filhantering/Differences.ps1
@@ -1,8 +1,30 @@
-$dpl = Get-Content -Path .\Desktop\dpl.csv
-$udpl = $dpl | Select-Object -Unique
-Compare-Object -ReferenceObject $udpl -DifferenceObject $dpl
+$dpl = Get-Content -Path .\Documents\dpl.csv
 
 
-$t = Get-Content -Path .\Desktop\Utedit.csv
+
+$t = Get-Content -Path .\Documents\Utedit.csv
 $x = $t | Select-Object -Unique
 Compare-Object -ReferenceObject $x -DifferenceObject $t
+
+foreach($dp in $dplo){
+    $dps = $dp.Split(" ")
+    $beteck = $dps[0]
+    $rad= $beteck + ";" + $dps
+    $rad | Out-File 2.csv -Append
+} 
+
+
+$2 = Import-csv .\2.csv -delimiter ";"
+
+
+
+foreach($zz in $2){
+    $Beteckningar += $zz.beteck + @"
+    
+
+"@
+    
+}
+
+$4 = Get-Content .\4.csv
+$4 | Select-Object -Unique | Out-File 5.csv
